497c7f1a0cfda76edda5255bae6711eeb31aa4e7
Programix-NaveJL <alfredo.naveju.lop@gmail.com>
2026-07-06 18:02:16 -0600

    docs: inicializar documentos de cumplimiento legal y políticas

--- android/app/src/main/kotlin/com/programixnavejl/aspirantes_itvh/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.programixnavejl.aspirantes_itvh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/programixnavejl/aspirantes_itvh_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.programixnavejl.aspirantes_itvh_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
